2f96165329b7102196d9e3ec20929812681c0d70
robocon321n@gmail.com <robocon321n@gmail.com>
2019-03-14 01:04:56 +0700

    sayHi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,3 +1,4 @@
+var chalk=require('chalk');
 function Dog()
 {
 	this.stomach=[];
@@ -5,3 +6,7 @@ function Dog()
 Dog.prototype.eat=function(cat){
 	this.stomach.push(cat);
 }
+Dog.prototype.sayHi=function(){
+	console.log("sayHi! I am Nhat"+chalk.red(this.name));
+}
+module.exports=Dog;
